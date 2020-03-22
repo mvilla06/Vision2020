c0515f436719fab03d7dc7846e2c0684827a4591
mvilla06 <mauricio.villalpando_06@hotmail.com>
2020-03-22 11:20:52 -0600

    Set max and min

--- Show-Image/src/clic.cpp
+++ Show-Image/src/clic.cpp
@@ -24,7 +24,8 @@ using namespace std;
 
 // Here we will store points
 Point p(0, 0), q(0, 0);
-
+Mat obj;
+char o = 0;
 
 
 /* This is the callback that will only display mouse coordinates */
@@ -33,7 +34,7 @@ void mouseCoordinatesExampleCallback(int event, int x, int y, int flags, void *p
 void histogram(const Mat &image, char t);
 
 void draw(Mat image, char t){
-    Mat obj;
+    Mat roi;
     Mat img = image.clone();
     if(!t){
         if(p.x>0 && p.y>0)
@@ -41,14 +42,16 @@ void draw(Mat image, char t){
     }
     else
     {
-        if(p!=q){
-                Rect roi(p, q);
-                obj = image(roi);
-                imshow("ROI", obj);
+        if(p!=q){//Valid ROI selection
+                Rect rec(p, q);
+                roi = image(rec);
+                imshow("ROI", roi);
                 rectangle(img, p, q, Scalar(0, 0, 255), 1, 8, 0);
+                obj = roi.clone();
+                o = 1;
                 }
     }
-        
+    
     imshow("Image", img);
 
 }
@@ -61,7 +64,7 @@ int main(int argc, char *argv[])
 
     /* Create images where captured and transformed frames are going to be stored */
     Mat currentImage;
-
+    Mat obj;
     /* Create main OpenCV window to attach callbacks */
     namedWindow("Image");
     setMouseCallback("Image", mouseCoordinatesExampleCallback);
@@ -88,23 +91,21 @@ int main(int argc, char *argv[])
 
             
             draw(currentImage, t);
-
+            
+            
             /* If 'x' is pressed, exit program */
             
             
             x = waitKey(3);
-            if (x == ' ')
+            if (x == ' ') //Still image
             {
                 if(!t)
                     q = p;
                 t = ~t;
 
-            } else if(x=='x') break;
-            else if (x=='h'){
+            } else if(x=='x') break; //End program
+            else if (x=='h'){   //Show histogram
                 h= ~h;
-            }
-            else if(x=='\n'){
-                if(t) break;
             }
         }
         else
@@ -165,6 +166,24 @@ void histogram(const Mat &image, char t)
             for (int k = 0; k < image.channels(); k++)
                 channels[k][image.at<Vec3b>(i, j)[k]]++;
 
+    //Calculate the maxima and minima of the ROI 
+    unsigned char * threshold = new unsigned char[obj.channels()*2];
+    memset(threshold, 0xff, obj.channels() * sizeof(char));
+    memset(threshold + obj.channels() * sizeof(char), 0x00, obj.channels() * sizeof(char));
+
+    
+    if (o)
+        for (int r = 0; r < obj.rows; r++)
+            for (int c = 0; c < obj.cols; c++)
+                for (int ch = 0; ch < obj.channels(); ch++)
+                {
+                    if (obj.at<Vec3b>(r, c)[ch] < threshold[ch])
+                        threshold[ch] = obj.at<Vec3b>(r, c)[ch];
+
+                    if (obj.at<Vec3b>(r, c)[ch] > threshold[ch + obj.channels()])
+                        threshold[ch + obj.channels()] = obj.at<Vec3b>(r, c)[ch];
+                }
+
     for (int i = 0; i < image.channels(); i++)
     {
         int max = 0;
@@ -183,18 +202,27 @@ void histogram(const Mat &image, char t)
             }
         }
 
-        if(!t){
-            //Plot a vertical bar in the position of the values of the point
-            for (int j = 0; j < 255; j++)
-            {
-                mats[i].at<char>(j, pv[i] * 2) = 0xff;
-                mats[i].at<char>(j, pv[i] * 2 + 1) = 0xff;
-            }
-        }else{
-
+        //Plot a vertical bar in the position of the values of the point
+        for (int j = 0; j < 255; j++)
+        {
+            mats[i].at<char>(j, pv[i] * 2) = 0x7f;
+            mats[i].at<char>(j, pv[i] * 2 + 1) = 0x7f;
         }
 
+        printf("%d\n",threshold[i+obj.channels()]);
+        if (o)
         
+        //Plot a vertical bar at the minimum and maximun of the ROI
+        for (int j = 0; j < 255; j++)
+        {
+            mats[i].at<char>(j, threshold[i] * 2) = 0x3f;
+            mats[i].at<char>(j, threshold[i] * 2 + 1) = 0x3f;
+            mats[i].at<char>(j, threshold[i+obj.channels()] * 2) = 0xff;
+            mats[i].at<char>(j, threshold[i+obj.channels()] * 2 + 1) = 0xff;
+            
+        }
+
+
         char title[20];
         sprintf(title, "Channel %d", i);
         imshow(title, mats[i]);
@@ -207,5 +235,6 @@ void histogram(const Mat &image, char t)
     }
     free(channels);
     delete [] mats;
-    
+    if (o)
+        delete[] threshold;
 }
